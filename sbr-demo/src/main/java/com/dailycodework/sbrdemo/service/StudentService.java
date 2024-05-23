f51c7e8e46739f984dc1ec99427be4dcfe391966
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-23 11:52:54 +0900

    src/main/java/dailycodework/sbrdemoにservice/StudentService.javaを作成する

--- sbr-demo/src/main/java/com/dailycodework/sbrdemo/service/StudentService.java
+++ sbr-demo/src/main/java/com/dailycodework/sbrdemo/service/StudentService.java
@@ -6,6 +6,11 @@ import org.springframework.stereotype.Service;
 public class StudentService implements IStudentService {
 
     @Override
+    public Student addStudent(Student student) {
+        return null;
+    }
+
+    @Override
     public List<Student> getStudents() {
         return null;
     }
