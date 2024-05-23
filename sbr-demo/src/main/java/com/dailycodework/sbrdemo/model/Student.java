77ea548b58f01000a60e53d1dfcee6597bcc2f99
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-23 11:00:06 +0900

    src/main/java/dailycodework/sbrdemoにmodel/Student.javaを作成する

--- sbr-demo/src/main/java/com/dailycodework/sbrdemo/model/Student.java
+++ sbr-demo/src/main/java/com/dailycodework/sbrdemo/model/Student.java
@@ -1,6 +1,8 @@
 package com.dailycodework.sbrdemo.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,10 +14,12 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Student {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
+    @NuturalId(mutable = true)
     private String email;
     private String department;
 
