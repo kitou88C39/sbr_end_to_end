b63dbfee766f22bdacd0a7e4445d6c7398ef1d5d
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-23 19:46:15 +0900

    src/main/java/dailycodework/sbrdemoにcontroller/StudentController.javaを作成する

--- sbr-demo/src/main/java/com/dailycodework/sbrdemo/controller/StudentController.java
+++ sbr-demo/src/main/java/com/dailycodework/sbrdemo/controller/StudentController.java
@@ -1,6 +1,8 @@
 package com.dailycodework.sbrdemo.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +16,13 @@ import lombok.RequiredArgsConstructor;
 public class StudentController {
     private final IStudentService studentService;
 
+    @GetMapping()
     public ResponseEntity<List<Student>> getStudents() {
+        return new ResponseEntity<>(studentService.getStudents(), HttpStatus.FOUND);
+    }
+
+    public Student addStudent(Student student) {
+
     }
 
 }
