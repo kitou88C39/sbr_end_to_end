6566ce1dd07f36c4035301ce695892e0f2bba55e
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-23 15:52:56 +0900

    src/main/java/dailycodework/sbrdemoにexception/StudentNotFoundException.javaを作成する

--- sbr-demo/src/main/java/com/dailycodework/sbrdemo/exception/StudentNotFoundException.java
+++ sbr-demo/src/main/java/com/dailycodework/sbrdemo/exception/StudentNotFoundException.java
@@ -1,5 +1,9 @@
 package com.dailycodework.sbrdemo.exception;
 
-public class StudentNotFoundException {
+public class StudentNotFoundException extends RuntimeException {
+
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
 
 }
