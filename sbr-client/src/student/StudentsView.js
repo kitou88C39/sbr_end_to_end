908faffe3e1f640b393c0e7362463dbed5539758
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-24 16:02:07 +0900

    src/student/StudentsView.jsを作成する

--- sbr-client/src/student/StudentsView.js
+++ sbr-client/src/student/StudentsView.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const StudentsView = () => {
+  const [student, setStudent] = useState([]);
   return (
     <section>
       <table>
         <thead>
           <tr>
-            <th></th>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Depatment</th>
+            <th>Actions</th>
           </tr>
         </thead>
+        <tbody>
+          <tr>
+            <td></td>
+          </tr>
+        </tbody>
       </table>
     </section>
   );
