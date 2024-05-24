f568726265deba44165f6894fa7f4e36d94dd48d
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-24 15:40:02 +0900

    src/App.jsを編集する

--- sbr-client/src/App.js
+++ sbr-client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Home from './Home';
 
 function App() {
   return (
     <div className='App'>
-      <h2></h2>
+      <h2>Welcome to our front end</h2>
+      <Home />
     </div>
   );
 }
