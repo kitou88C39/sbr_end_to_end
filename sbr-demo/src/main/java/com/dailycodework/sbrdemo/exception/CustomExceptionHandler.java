576a87aa76ab5bd5e6ade63eea6a3219985cdcd9
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-24 14:00:32 +0900

    src/main/java/dailycodework/sbrdemoにexception/ CustomExceptionHandler.javaを作成する

--- sbr-demo/src/main/java/com/dailycodework/sbrdemo/exception/CustomExceptionHandler.java
+++ sbr-demo/src/main/java/com/dailycodework/sbrdemo/exception/CustomExceptionHandler.java
@@ -10,27 +10,27 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)@ExceptionHandler(MethodExceptionHandler.class){
-
-    public Map<String, String> handleException(MethodArgumentNotValidException ex)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleException(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult()BindingResult
-            .getFieldErrors()List<FikdError>
-            .forEach(error -> errors.put(error.getFild(), error.getDefaultMessage()));
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
         return errors;
     }
 
-}@ResponseStatus(HttpStatus.NOT_FOUND)@ExceptionHandler(MethodExceptionHandler.class){
-
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(StudentNotFoundException.class)
     public Map<String, String> userNotFound(StudentNotFoundException ex) {
         Map<String, String> errors = new HashMap<>();
         errors.put("error", ex.getMessage());
         return errors;
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)@ExceptionHandler(StudentAlreadyExistsException.class){
-
-    public Map<String, String> userNotFound(StudentAlreadyExistsException ex) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(StudentAlreadyExistsException.class)
+    public Map<String, String> userAlreadyExists(StudentAlreadyExistsException ex) {
         Map<String, String> errors = new HashMap<>();
         errors.put("error", ex.getMessage());
         return errors;
