6e9edbce814e95defcda0276dfd0e2e140cda12d
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-24 12:18:36 +0900

    src/main/java/dailycodework/sbrdemoにexception/ CustomExceptionHandler.javaを作成する

--- sbr-demo/src/main/java/com/dailycodework/sbrdemo/exception/CustomExceptionHandler.java
+++ sbr-demo/src/main/java/com/dailycodework/sbrdemo/exception/CustomExceptionHandler.java
@@ -1,5 +1,12 @@
 package com.dailycodework.sbrdemo.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class CustomExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
 
 }
