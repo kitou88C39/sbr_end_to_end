8c9b3c85e4175ce5d1af734e02c5931ecf548f4e
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-24 12:22:44 +0900

    src/main/java/dailycodework/sbrdemoにexception/ CustomExceptionHandler.javaを作成する

--- sbr-demo/src/main/java/com/dailycodework/sbrdemo/exception/CustomExceptionHandler.java
+++ sbr-demo/src/main/java/com/dailycodework/sbrdemo/exception/CustomExceptionHandler.java
@@ -1,12 +1,19 @@
 package com.dailycodework.sbrdemo.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)@ExceptionHandler(MethodExceptionHandler.class){
+
+    public Map<String, String> handleException(MethodArgumentNotValidException)
+        Map<String, String> errors = new HashMap<>();
+
+    }
 
 }
