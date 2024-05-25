09c4bf0d8b14e86a9f14a4450d2ef29584f21e3d
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-25 15:51:59 +0900

    src/componets/student/AddStudent.jsを作成する

--- sbr-client/src/components/student/AddStudent.js
+++ sbr-client/src/components/student/AddStudent.js
@@ -8,7 +8,28 @@ const AddStudent = () => {
           <label className='input-group-text' htmlFor='FirstName'>
             First Name
           </label>
-          <input className='form-controll col-sm-6'></input>
+          <input
+            className='form-controll col-sm-6'
+            type='text'
+            name='firstName'
+            id='firstName'
+            required
+            value={firstName}
+          />
+        </div>
+
+        <div className='input-group mb-5'>
+          <label className='input-group-text' htmlFor='FirstName'>
+            First Name
+          </label>
+          <input
+            className='form-controll col-sm-6'
+            type='text'
+            name='firstName'
+            id='firstName'
+            required
+            value={firstName}
+          />
         </div>
       </from>
     </div>
