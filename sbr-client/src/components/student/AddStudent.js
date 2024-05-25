afa9152604e96371b18ff96f5e5ab261f06d048c
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-25 20:23:21 +0900

    src/componets/student/AddStudent.jsを作成する

--- sbr-client/src/components/student/AddStudent.js
+++ sbr-client/src/components/student/AddStudent.js
@@ -16,12 +16,13 @@ const AddStudent = () => {
   };
 
   const savaStudent = async () => {
-    await axios.post('');
+    e.preventDefault();
+    await axios.post('http://localhost:9192/students', student);
   };
 
   return (
     <div className='col-sm-8 py-2 px-5'>
-      <from>
+      <from onChange={(e) => savaStudent(e)}>
         <div className='input-group mb-5'>
           <label className='input-group-text' htmlFor='FirstName'>
             First Name
