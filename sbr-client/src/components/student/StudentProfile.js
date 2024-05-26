61e279c04fbe9c59f222b24eabf6b0d6c5122942
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-26 16:14:00 +0900

    src/componets/student/StudentProfile.jsを作成する

--- sbr-client/src/components/student/StudentProfile.js
+++ sbr-client/src/components/student/StudentProfile.js
@@ -4,7 +4,20 @@ const StudentProfile = () => {
   return;
   <section style={{ backgroudColor: 'whitesmoke' }}>
     <div className='container py-5'>
-      <div className='row'></div>
+      <div className='row'>
+        <div className='col-lg-3'>
+          <div className='card mb-4'>
+            <div className='card-body text-center'>
+              <img
+                src='https://mdbcdn.b-cdn.net/img/Photos/new-template'
+                alt='avatar'
+                className='rounded-circle img-fluid'
+                style={{ width150 }}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </section>;
 };
