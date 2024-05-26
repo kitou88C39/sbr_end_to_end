5a781b5460abd8e1f41e7dfff920c60989eea925
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-26 16:36:16 +0900

    src/componets/student/StudentProfile.jsを作成する

--- sbr-client/src/components/student/StudentProfile.js
+++ sbr-client/src/components/student/StudentProfile.js
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const [student, setStudent] = useState([]);
 
 const StudentProfile = () => {
-  return;
-  <section style={{ backgroudColor: 'whitesmoke' }}>
-    <div className='container py-5'>
-      <div className='row'>
-        <div className='col-lg-3'>
-          <div className='card mb-4'>
-            <div className='card-body text-center'>
-              <img
-                src='https://mdbcdn.b-cdn.net/img/Photos/new-template'
-                alt='avatar'
-                className='rounded-circle img-fluid'
-                style={{ width150 }}
-              />
+  return (
+    <section style={{ backgroudColor: 'whitesmoke' }}>
+      <div className='container py-5'>
+        <div className='row'>
+          <div className='col-lg-3'>
+            <div className='card mb-4'>
+              <div className='card-body text-center'>
+                <img
+                  src='https://mdbcdn.b-cdn.net/img/Photos/new-template'
+                  alt='avatar'
+                  className='rounded-circle img-fluid'
+                  style={{ width: 150 }}
+                />
+                <div className='col-sm-9'>
+                  <h5 className='mb-0'>Department</h5>
+                </div>
+
+                <div className='col-sm-9'>
+                  <p className='text-muted mb-0'>{student.department}</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>;
+    </section>
+  );
 };
 
 export default StudentProfile;
