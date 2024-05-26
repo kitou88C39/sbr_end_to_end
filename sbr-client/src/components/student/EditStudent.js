8c49b6cab5ac675332a1c6226b342cdd7aeed02d
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-26 14:29:25 +0900

    src/componets/student/EditStudent.jsを作成する

--- sbr-client/src/components/student/EditStudent.js
+++ sbr-client/src/components/student/EditStudent.js
@@ -1,7 +1,105 @@
 import React from 'react';
 
 const EditStudent = () => {
-  return <div>EditStudent</div>;
+  return let navigate = useNavigate();
+  const [student, setStudent] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    department: '',
+  });
+  const { firstName, lastName, email, department } = student;
+
+  const handleInputChange = (e) => {
+    setStudent({ ...student, [e.target.name]: e.target.value });
+  };
+
+  const savaStudent = async (e) => {
+    e.preventDefault();
+    await axios.post('http://localhost:9192/students', student);
+    navigate('/view-students');
+  };
+
+  return (
+    <div className='col-sm-8 py-2 px-5'>
+      <from onChange={(e) => savaStudent(e)}>
+        <div className='input-group mb-5'>
+          <label className='input-group-text' htmlFor='FirstName'>
+            First Name
+          </label>
+          <input
+            className='form-controll col-sm-6'
+            type='text'
+            name='firstName'
+            id='firstName'
+            required
+            value={firstName}
+            onChange={(e) => handleInputChange(e)}
+          />
+        </div>
+
+        <div className='input-group mb-5'>
+          <label className='input-group-text' htmlFor='lastName'>
+            Last Name
+          </label>
+          <input
+            className='form-controll col-sm-6'
+            type='text'
+            name='lastName'
+            id='lastName'
+            required
+            value={lastName}
+            onChange={(e) => handleInputChange(e)}
+          />
+        </div>
+
+        <div className='input-group mb-5'>
+          <label className='input-group-text' htmlFor='email'>
+            Email
+          </label>
+          <input
+            className='form-controll col-sm-6'
+            type='email'
+            name='email'
+            id='email'
+            required
+            value={email}
+            onChange={(e) => handleInputChange(e)}
+          />
+        </div>
+
+        <div className='input-group mb-5'>
+          <label className='input-group-text' htmlFor='department'>
+            Department
+          </label>
+          <input
+            className='form-controll col-sm-6'
+            type='text'
+            name='department'
+            id='department'
+            required
+            value={department}
+            onChange={(e) => handleInputChange(e)}
+          />
+        </div>
+
+        <div className='col-sm-2'>
+          <button type='submit' className='btn btn-outline-warning btn-lg'>
+            Save
+          </button>
+        </div>
+        <div className='col-sm-2'>
+          <Link
+            to={'/view-students'}
+            type='submit'
+            className='btn btn-outline-warning btn-lg'
+          >
+            Cancel
+          </Link>
+        </div>
+      </from>
+    </div>
+  );
 };
 
 export default EditStudent;
