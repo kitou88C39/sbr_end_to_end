1aee7f46ce8c95d737afe5af7b53521dc8f9c5b0
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-27 12:01:21 +0900

    src/componets/student/StudentsView.jsを作成する

--- sbr-client/src/components/student/StudentsView.js
+++ sbr-client/src/components/student/StudentsView.js
@@ -6,6 +6,7 @@ import Search from '../common/Search';
 
 const StudentsView = () => {
   const [students, setStudents] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     loadStudents();
@@ -43,41 +44,43 @@ const StudentsView = () => {
         </thead>
 
         <tbody className='text-center'>
-          {students((student, index) => (
-            <tr key={student.id}>
-              <th scope='row' key={index}>
-                {index + 1}
-              </th>
-              <td>{student.firstName}</td>
-              <td>{student.lastName}</td>
-              <td>{student.email}</td>
-              <td>{student.deparment}</td>
-              <td className='mx-2'>
-                <Link
-                  to={`/student-profile/${student.id}`}
-                  className='btn btn-info'
-                >
-                  <FaEye />
-                </Link>
-              </td>
-              <td className='mx-2'>
-                <Link
-                  to={`/edit-student/${student.id}`}
-                  className='btn btn-warning'
-                >
-                  <FaEdit />
-                </Link>
-              </td>
-              <td className='mx-2'>
-                <button
-                  className='btn btn-danger'
-                  onChange={() => handleDelete(student.id)}
-                >
-                  <FaTrashAlt />
-                </button>
-              </td>
-            </tr>
-          ))}
+          {students
+            .fill((st) => st.firstName.toLowerCase().includes(search))
+            .map((student, index) => (
+              <tr key={student.id}>
+                <th scope='row' key={index}>
+                  {index + 1}
+                </th>
+                <td>{student.firstName}</td>
+                <td>{student.lastName}</td>
+                <td>{student.email}</td>
+                <td>{student.deparment}</td>
+                <td className='mx-2'>
+                  <Link
+                    to={`/student-profile/${student.id}`}
+                    className='btn btn-info'
+                  >
+                    <FaEye />
+                  </Link>
+                </td>
+                <td className='mx-2'>
+                  <Link
+                    to={`/edit-student/${student.id}`}
+                    className='btn btn-warning'
+                  >
+                    <FaEdit />
+                  </Link>
+                </td>
+                <td className='mx-2'>
+                  <button
+                    className='btn btn-danger'
+                    onChange={() => handleDelete(student.id)}
+                  >
+                    <FaTrashAlt />
+                  </button>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </section>
