1bc647974d98048b4a63b4bf55a54681e5d72dee
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-27 10:19:23 +0900

    src/componets/student/StudentProfile.jsを作成する

--- sbr-client/src/components/student/StudentProfile.js
+++ sbr-client/src/components/student/StudentProfile.js
@@ -49,10 +49,10 @@ const StudentProfile = () => {
                   <div className='col-sm-3'>
                     <h5 className='mb-0'>First Name</h5>
                   </div>
-                </div>
 
-                <div className='col-sm-9'>
-                  <p className='text-muted mb-0'>{student.firstName}</p>
+                  <div className='col-sm-9'>
+                    <p className='text-muted mb-0'>{student.firstName}</p>
+                  </div>
                 </div>
                 <hr />
 
@@ -60,10 +60,10 @@ const StudentProfile = () => {
                   <div className='col-sm-3'>
                     <h5 className='mb-0'>Last Name</h5>
                   </div>
-                </div>
 
-                <div className='col-sm-9'>
-                  <p className='text-muted mb-0'>{student.lastName}</p>
+                  <div className='col-sm-9'>
+                    <p className='text-muted mb-0'>{student.lastName}</p>
+                  </div>
                 </div>
               </div>
               <hr />
@@ -72,9 +72,9 @@ const StudentProfile = () => {
                 <div className='col-sm-3'>
                   <h5 className='mb-0'>Email</h5>
                 </div>
-              </div>
-              <div className='col-sm-9'>
-                <p className='text-muted mb-0'>{student.email}</p>
+                <div className='col-sm-9'>
+                  <p className='text-muted mb-0'>{student.email}</p>
+                </div>
               </div>
               <hr />
 
@@ -82,10 +82,10 @@ const StudentProfile = () => {
                 <div className='col-sm-9'>
                   <h5 className='mb-0'>Department</h5>
                 </div>
-              </div>
 
-              <div className='col-sm-9'>
-                <p className='text-muted mb-0'>{student.department}</p>
+                <div className='col-sm-9'>
+                  <p className='text-muted mb-0'>{student.department}</p>
+                </div>
               </div>
             </div>
           </div>
