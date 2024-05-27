e47ceb9b8a4ae4e1b459a587efb1e2466fe1a2f0
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-27 10:14:59 +0900

    src/componets/student/StudentProfile.jsを作成する

--- sbr-client/src/components/student/StudentProfile.js
+++ sbr-client/src/components/student/StudentProfile.js
@@ -17,7 +17,7 @@ const StudentProfile = () => {
             <div className='card mb-4'>
               <div className='card-body text-center'>
                 <img
-                  src='https://mdbcdn.b-cdn.net/img/Photos/new-template/boostrap-chat'
+                  src='https://mdbcdn.b-cdn.net/img/Photos/new-template/boostrap-chat/avatar'
                   alt='avatar'
                   className='rounded-circle img-fluid'
                   style={{ width: 150 }}
@@ -40,47 +40,55 @@ const StudentProfile = () => {
             </div>
           </div>
 
-          <div className='row'>
-            <div className='col-sm-3'>
-              <h5 className='mb-0'>First Name</h5>
+          <div className='col-lg-9'>
+            <div className='card mb-4'>
+              <div className='card-body'>
+                <hr />
+
+                <div className='row'>
+                  <div className='col-sm-3'>
+                    <h5 className='mb-0'>First Name</h5>
+                  </div>
+                </div>
+
+                <div className='col-sm-9'>
+                  <p className='text-muted mb-0'>{student.firstName}</p>
+                </div>
+                <hr />
+
+                <div className='row'>
+                  <div className='col-sm-3'>
+                    <h5 className='mb-0'>Last Name</h5>
+                  </div>
+                </div>
+
+                <div className='col-sm-9'>
+                  <p className='text-muted mb-0'>{student.lastName}</p>
+                </div>
+              </div>
+              <hr />
+
+              <div className='row'>
+                <div className='col-sm-3'>
+                  <h5 className='mb-0'>Email</h5>
+                </div>
+              </div>
+              <div className='col-sm-9'>
+                <p className='text-muted mb-0'>{student.email}</p>
+              </div>
+              <hr />
+
+              <div className='row'>
+                <div className='col-sm-9'>
+                  <h5 className='mb-0'>Department</h5>
+                </div>
+              </div>
+
+              <div className='col-sm-9'>
+                <p className='text-muted mb-0'>{student.department}</p>
+              </div>
             </div>
           </div>
-
-          <div className='col-sm-9'>
-            <p className='text-muted mb-0'>{student.firstName}</p>
-          </div>
-          <hr />
-
-          <div className='row'>
-            <div className='col-sm-3'>
-              <h5 className='mb-0'>Last Name</h5>
-            </div>
-          </div>
-
-          <div className='col-sm-9'>
-            <p className='text-muted mb-0'>{student.lastName}</p>
-          </div>
-        </div>
-        <hr />
-
-        <div className='row'>
-          <div className='col-sm-3'>
-            <h5 className='mb-0'>Email</h5>
-          </div>
-        </div>
-        <div className='col-sm-9'>
-          <p className='text-muted mb-0'>{student.email}</p>
-        </div>
-        <hr />
-
-        <div className='row'>
-          <div className='col-sm-9'>
-            <h5 className='mb-0'>Department</h5>
-          </div>
-        </div>
-
-        <div className='col-sm-9'>
-          <p className='text-muted mb-0'>{student.department}</p>
         </div>
       </div>
     </section>
